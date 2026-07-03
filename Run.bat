4f4d8ee2707af7002a4cff5e868d83371637ab59
MARCOS KAZIMOTO <marcos.kazimoto@totvs.com.br>
2026-07-03 09:14:39 -0300

    fix: correct order of commands in Run.bat to ensure proper server shutdown and startup

--- Run.bat
+++ Run.bat
@@ -1,8 +1,8 @@
 cd "C:\git\ExtrairRAG-BancoDados"
 
+net stop "TotvsRmDatabaseMcpServer2"
 cmd.exe /c "git fetch"
 cmd.exe /c "git pull"
 cmd.exe /c "npm install"
 cmd.exe /c "npm run extract"
-net stop "TotvsRmDatabaseMcpServer2"
 net start "TotvsRmDatabaseMcpServer2"
